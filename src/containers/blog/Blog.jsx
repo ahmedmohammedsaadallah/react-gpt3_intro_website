62d6d87de48aca06ce7afaa991b7b36f17d80e8f
Ahmed Mohammed Saadallah <ahmedmohammedsaadallah@gmail.com>
2023-03-21 16:19:40 +0200

    first commit

---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,46 @@
+import "./blog.css";
+import Article from "../../components/article/Article.jsx";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports.js";
+const Blog = () => {
+  return (
+    <div className="gpt3__blog section__padding">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, <br /> We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article
+            imgUrl={blog01}
+            date="sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article
+            imgUrl={blog02}
+            date="sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog03}
+            date="sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog04}
+            date="sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog05}
+            date="sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Blog;
